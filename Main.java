8de9a911ea61f88097998c9006815a3eba4e2c95
Mikael Borges de Oliveira e Silva Junior <114350039+kamamijr@users.noreply.github.com>
2023-05-03 19:30:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+class Pessoa{
+    private short dia;
+    private short mes;
+    private short ano;
+    public short idade;
+}
+
+class Pessoa{
+    
+}
+
+public class Pessoa{
+ private Data nascimento;
+ private String nome;
+}
+class Livro{
+    
+}
+
+public class Prova {
+    private byte numQuestoes;
+    
+    public byte getNumQuestoes(){
+        return numQuestoes ;
+    }
+    
+    public void setNumQuestoes(byte n) {
+        if(n > 0) {
+            numQuestoes = n;
+        }
+    }
+}
+
+
+public class Pessoa {
+    private Data nascimento;
+    private String nome;
+    private Pessoa pai;
+    private Pessoa mae;
+}
+
+public class Livro extends Object {
+    
+}
+
+public class TesteProva {
+    public static void main(String[]args)
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
