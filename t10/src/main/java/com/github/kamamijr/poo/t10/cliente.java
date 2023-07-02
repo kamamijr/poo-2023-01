bb6cb7232ca5c92b3f68f5e0d7183fc8e904b959
Mikael Borges de Oliveira e Silva Junior <114350039+kamamijr@users.noreply.github.com>
2023-07-02 17:47:53 -0300

    t10
    arrumando classes e uml

--- t10/src/main/java/com/github/kamamijr/poo/t10/cliente.java
+++ t10/src/main/java/com/github/kamamijr/poo/t10/cliente.java
@@ -1,40 +1,152 @@
-package com.github.kamamijr.main.t10;
+package com.github.kamamijr.poo.t10;
 
-class Cliente{
-    private String nome;
-    private Demanda problema;
-    private Bicicleta bicicleta;
+import java.util.ArrayList;
+import java.util.List;
 
-    public void cadastrarCliente(){
-        cliente = new Cliente();
-        this.cadastrarCliente("mikael", "Bicicleta com pneu furado", "brx", "amarela", 500);
+// Classe principal que representa uma bicicleta
+class Bike {
+    private String modelo;
+    private String marca;
+    private int ano;
+
+    public Bike(String modelo, String marca, int ano) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
     }
 
-    public void cadastrarCliente(String nome, String descricao, String modelo, String cor, float preco){
-        this.nome = nome;
-        this.problema = new Demanda();
-        this.problema.DefinirDemanda(descricao);
-        this.bicicleta = new Bicicleta();
-        this.bicicleta.DescricaoBicicleta(modelo, cor, preco);
+    // Getters e setters
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
     }
 }
 
-class Bicicleta{
-    private String modelo;
-    private String cor;
-    private float preco;
+// Classe que representa um serviço de manutenção
+class ServicoManutencao {
+    private String nome;
+    private double preco;
 
-    public void DescricaoBicicleta(String modelo, String cor, float preco){
-        this.modelo = modelo;
-        this.cor = cor;
+    public ServicoManutencao(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    // Getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 }
 
-class Demanda{
-    private String descricao;
+// Classe que representa uma oficina
+class Oficina {
+    private List<Bike> bikes;
+    private List<ServicoManutencao> servicos;
 
-    public void DefinirDemanda(String descricao){
-        this.descricao = descricao;
+    public Oficina() {
+        bikes = new ArrayList<>();
+        servicos = new ArrayList<>();
+    }
+
+    // Métodos para adicionar e remover bicicletas e serviços
+
+    public void adicionarBike(Bike bike) {
+        bikes.add(bike);
+    }
+
+    public void removerBike(Bike bike) {
+        bikes.remove(bike);
+    }
+
+    public void adicionarServico(ServicoManutencao servico) {
+        servicos.add(servico);
+    }
+
+    public void removerServico(ServicoManutencao servico) {
+        servicos.remove(servico);
+    }
+
+    // Outros métodos para a oficina
+
+    public void realizarManutencao(Bike bike, ServicoManutencao servico) {
+        // Lógica para realizar a manutenção na bicicleta usando o serviço especificado
+        System.out.println("Realizando manutenção na bike " + bike.getModelo() + " com o serviço " + servico.getNome());
+        System.out.println("Preço: R$" + servico.getPreco());
+    }
+
+    public void listarBikes() {
+        System.out.println("Bicicletas na oficina:");
+        for (Bike bike : bikes) {
+            System.out.println("- " + bike.getModelo() + " (" + bike.getMarca() + ")");
+        }
+    }
+
+    public void listarServicos() {
+        System.out.println("Serviços disponíveis:");
+        for (ServicoManutencao servico : servicos) {
+            System.out.println("- " + servico.getNome() + " (R$" + servico.getPreco() + ")");
+        }
+    }
+}
+
+// Classe de teste
+class Main {
+    public static void main(String[] args) {
+        // Criando instâncias de bicicletas
+        Bike bike1 = new Bike("Speed", "Caloi", 2020);
+        Bike bike2 = new Bike("Mountain", "Trek", 2021);
+
+        // Criando instâncias de serviços de manutenção
+        ServicoManutencao servico1 = new ServicoManutencao("Troca de pneus", 50.0);
+        ServicoManutencao servico2 = new ServicoManutencao("Ajuste de freios", 30.0);
+
+        // Criando uma instância da oficina
+        Oficina oficina = new Oficina();
+
+        // Adicionando bicicletas e serviços na oficina
+        oficina.adicionarBike(bike1);
+        oficina.adicionarBike(bike2);
+        oficina.adicionarServico(servico1);
+        oficina.adicionarServico(servico2);
+
+        // Realizando uma manutenção na primeira bicicleta usando o primeiro serviço
+        oficina.realizarManutencao(bike1, servico1);
+
+        // Listando as bicicletas e os serviços disponíveis na oficina
+        oficina.listarBikes();
+        oficina.listarServicos();
     }
 }
